e34cde8495fc954595154660b0b225e44e44e4d1
zvermandr <zvermandr@gmail.com>
2017-03-06 19:09:57 +0300

    Add files via upload

---
+++ KG_Lab2/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace KG_Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbOriginalImage = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pbNewImage = new System.Windows.Forms.PictureBox();
+            this.processButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbNewColor = new System.Windows.Forms.PictureBox();
+            this.chooseColorButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbOldColor = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sensetiveTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginalImage)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNewImage)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNewColor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOldColor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sensetiveTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbOriginalImage
+            // 
+            this.pbOriginalImage.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pbOriginalImage.Location = new System.Drawing.Point(13, 42);
+            this.pbOriginalImage.Name = "pbOriginalImage";
+            this.pbOriginalImage.Size = new System.Drawing.Size(426, 354);
+            this.pbOriginalImage.TabIndex = 1;
+            this.pbOriginalImage.TabStop = false;
+            this.pbOriginalImage.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chooseColor);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(1113, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.fileToolStripMenuItem.Text = "Image";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.openToolStripMenuItem.Text = "Load new";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // pbNewImage
+            // 
+            this.pbNewImage.Location = new System.Drawing.Point(671, 42);
+            this.pbNewImage.Name = "pbNewImage";
+            this.pbNewImage.Size = new System.Drawing.Size(442, 354);
+            this.pbNewImage.TabIndex = 3;
+            this.pbNewImage.TabStop = false;
+            // 
+            // processButton
+            // 
+            this.processButton.Enabled = false;
+            this.processButton.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.processButton.Location = new System.Drawing.Point(499, 412);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(111, 38);
+            this.processButton.TabIndex = 4;
+            this.processButton.Text = "Process";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(356, 456);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(405, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.sensetiveTrackBar);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pbNewColor);
+            this.panel1.Controls.Add(this.chooseColorButton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pbOldColor);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(458, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(195, 354);
+            this.panel1.TabIndex = 6;
+            // 
+            // pbNewColor
+            // 
+            this.pbNewColor.Location = new System.Drawing.Point(29, 156);
+            this.pbNewColor.Name = "pbNewColor";
+            this.pbNewColor.Size = new System.Drawing.Size(141, 68);
+            this.pbNewColor.TabIndex = 4;
+            this.pbNewColor.TabStop = false;
+            // 
+            // chooseColorButton
+            // 
+            this.chooseColorButton.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chooseColorButton.Location = new System.Drawing.Point(29, 240);
+            this.chooseColorButton.Name = "chooseColorButton";
+            this.chooseColorButton.Size = new System.Drawing.Size(141, 33);
+            this.chooseColorButton.TabIndex = 3;
+            this.chooseColorButton.Text = "Choose new color";
+            this.chooseColorButton.UseVisualStyleBackColor = true;
+            this.chooseColorButton.Click += new System.EventHandler(this.ChooseColorButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "New";
+            // 
+            // pbOldColor
+            // 
+            this.pbOldColor.Location = new System.Drawing.Point(29, 45);
+            this.pbOldColor.Name = "pbOldColor";
+            this.pbOldColor.Size = new System.Drawing.Size(141, 66);
+            this.pbOldColor.TabIndex = 1;
+            this.pbOldColor.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Old";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sensetive";
+            // 
+            // sensetiveTrackBar
+            // 
+            this.sensetiveTrackBar.LargeChange = 10;
+            this.sensetiveTrackBar.Location = new System.Drawing.Point(29, 307);
+            this.sensetiveTrackBar.Maximum = 50;
+            this.sensetiveTrackBar.Name = "sensetiveTrackBar";
+            this.sensetiveTrackBar.Size = new System.Drawing.Size(141, 45);
+            this.sensetiveTrackBar.TabIndex = 6;
+            this.sensetiveTrackBar.TickFrequency = 5;
+            this.sensetiveTrackBar.Value = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 491);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.pbNewImage);
+            this.Controls.Add(this.pbOriginalImage);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change color in picture";
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginalImage)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNewImage)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNewColor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOldColor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sensetiveTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbOriginalImage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pbNewImage;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbOldColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pbNewColor;
+        private System.Windows.Forms.Button chooseColorButton;
+        private System.Windows.Forms.TrackBar sensetiveTrackBar;
+        private System.Windows.Forms.Label label3;
+    }
+};
+
---
+++ KG_Lab2/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KG_Lab2
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImageBitmap;
+
+        private bool isOpenImage = false;
+
+        private bool isChooseOldColor = false;
+        private bool isChooseNewColor = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = Environment.CurrentDirectory;
+            ofd.Filter = "All files (*.*)|*.*";
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(ofd.FileName);
+
+                Bitmap tmpBitmap = new Bitmap(image);
+                originalImageBitmap = new Bitmap(tmpBitmap, pbOriginalImage.Width, pbOriginalImage.Height);
+
+                pbOriginalImage.Image = originalImageBitmap;
+
+                pbOriginalImage.Cursor = System.Windows.Forms.Cursors.Hand;
+                isOpenImage = true;
+                //processingBtn.Enabled = true;
+            }
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            if (isOpenImage)
+            {
+                this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+                Bitmap layerOrig = (Bitmap)pbOriginalImage.Image;
+                Bitmap layerNew = new Bitmap(layerOrig.Width, layerOrig.Height);
+                processButton.Enabled = false;
+                chooseColorButton.Enabled = false;
+
+                progressBar1.Maximum = layerOrig.Height - 1;
+
+                TransformationRGBtoLUV oldLUV = getLUVbyColor(pbOldColor.BackColor);
+                TransformationRGBtoLUV newLUV = getLUVbyColor(pbNewColor.BackColor);
+                TransformationRGBtoLUV defLUV = getDef(oldLUV, newLUV);
+
+                for (int y = 0; y < layerOrig.Height; y++)
+                {
+                    for (int x = 0; x < layerOrig.Width; x++)
+                    {
+                        Color pixel = layerOrig.GetPixel(x, y);
+
+                        TransformationRGBtoLUV pixelLUV = getLUVbyColor(pixel);
+
+                        if (isChangedPixel(pixelLUV, oldLUV, sensetiveTrackBar.Value))
+                        {
+                            layerNew.SetPixel(x, y, findNewPixel(pixelLUV, defLUV));
+                        }
+                        else
+                        {
+                            layerNew.SetPixel(x, y, pixel);
+                        }
+
+                        pbNewImage.Image = layerNew;
+                    }
+                    progressBar1.Value = y;
+                }
+
+                this.Cursor = System.Windows.Forms.Cursors.Default;
+                processButton.Enabled = true;
+                chooseColorButton.Enabled = true;
+            }
+        }
+
+        private TransformationRGBtoLUV getLUVbyColor(Color color)
+        {
+            TransformationRGBtoLUV trans = new TransformationRGBtoLUV(Convert.ToString(color.R),
+                Convert.ToString(color.G), Convert.ToString(color.B));
+            return trans;
+        }
+
+        private TransformationRGBtoLUV getDef(TransformationRGBtoLUV oldColor, TransformationRGBtoLUV newColor)
+        {
+            TransformationRGBtoLUV def = new TransformationRGBtoLUV("0", "0", "0");
+            def.setL(oldColor.getL() - newColor.getL());
+            def.setU(oldColor.getU() - newColor.getU());
+            def.setV(oldColor.getV() - newColor.getV());
+            return def;
+        }
+
+        private bool isChangedPixel(TransformationRGBtoLUV pixel, TransformationRGBtoLUV newLUV, int senset)
+        {
+            double def1 = Math.Pow((pixel.getL() - newLUV.getL()), 2);
+            double def2 = Math.Pow(pixel.getU() - newLUV.getU(), 2);
+            double def3 = Math.Pow(pixel.getV() - newLUV.getV(), 2);
+
+            double def = Math.Pow(def1 + def2 + def3, 0.5);
+
+            if (def < senset)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private Color findNewPixel(TransformationRGBtoLUV pixelLUV, TransformationRGBtoLUV def)
+        {
+            TransformationRGBtoLUV newPixel = new TransformationRGBtoLUV("0", "0", "0");
+
+            newPixel.setL(pixelLUV.getL() - def.getL());
+            newPixel.setU(pixelLUV.getU() - def.getU());
+            newPixel.setV(pixelLUV.getV() - def.getV());
+
+            TransformationLUVtoRGB newPixelRGB = new TransformationLUVtoRGB(Convert.ToString(newPixel.getL()),
+                Convert.ToString(newPixel.getU()), Convert.ToString(newPixel.getV()));
+            
+            int red = newPixelRGB.getRed();
+            int green = newPixelRGB.getGreen();
+            int blue = newPixelRGB.getBlue();
+            if (red < 0)
+                red = 0;
+            else if (red > 255)
+                red = 255;
+            if (blue < 0)
+                blue = 0;
+            else if (blue > 255)
+                blue = 255;
+            if (green < 0)
+                green = 0;
+            else if (green > 255)
+                green = 255;
+            Color pix = Color.FromArgb(255, red, green , blue);
+            return pix;
+        }
+
+        private void chooseColor(object sender, MouseEventArgs e)
+        {
+            if (isOpenImage)
+            {
+                Bitmap bmp = (Bitmap)pbOriginalImage.Image;
+                pbOldColor.BackColor = Color.FromArgb(bmp.GetPixel(e.X, e.Y).ToArgb());
+                isChooseOldColor = true;
+                if (isChooseNewColor && isChooseOldColor)
+                {
+                    processButton.Enabled = true;
+                }
+            }
+        }
+
+        private void ChooseColorButton_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pbNewColor.BackColor = colorDialog1.Color;
+                isChooseNewColor = true;
+                if (isChooseNewColor && isChooseOldColor)
+                {
+                    processButton.Enabled = true;
+                }
+            }
+        }
+    }
+}
